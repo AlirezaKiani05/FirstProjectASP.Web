24b4ea82adfa988b32a41c40f5f48679b4b3da3a
alireza kiani <akiani083@gmail.com>
2026-05-30 11:53:54 +0330

    Add project files.

---
+++ FirstProjectASP.Web/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using FirstProjectASP.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProjectASP.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+
+        {
+            var data = new TestViewModel
+            {
+                Name = "Alireza",
+                DateOfBirth = new DateTime(1954,12,01)
+            };
+            
+            return View(data);
+        }
+    }
+}
---
+++ FirstProjectASP.Web/Models/TestViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstProjectASP.Web.Models
+{
+    public class TestViewModel
+    {
+        public string ? Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+    }
+}
